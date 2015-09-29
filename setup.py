8c80d61a9ca9c31c87cfd91c3bb4f80aa942da4c
fedemotta <fedemotta@gmail.com>
2015-09-29 18:56:00 -0300

    Adding pypi setup files

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'subseek',
+  packages = ['subseek'],
+  version = '2.0',
+  description = 'Search subtitles for all your movies and series with a single command line script',
+  author = 'Federico Nicolás Motta',
+  author_email = 'fedemotta@gmail.com',
+  url = 'https://github.com/fedemotta/subseek',
+  download_url = 'https://github.com/fedemotta/subseek/tarball/2.0',
+  keywords = ['subtitles', 'movies', 'series','batch','script'],
+  classifiers = [],
+)
