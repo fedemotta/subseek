7aad46bbe5f13d42d565ce1c71373169bf35d79e
fedemotta <fedemotta@gmail.com>
2015-09-29 19:04:31 -0300

    Pypi not liking non ascii code

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
   packages = ['subseek'],
   version = '2.0',
   description = 'Search subtitles for all your movies and series with a single command line script',
-  author = 'Federico Nicolás Motta',
+  author = 'Federico Nicolas Motta',
   author_email = 'fedemotta@gmail.com',
   url = 'https://github.com/fedemotta/subseek',
   download_url = 'https://github.com/fedemotta/subseek/tarball/2.0',
