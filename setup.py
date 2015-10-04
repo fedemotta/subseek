fecf888fae3255761948cdd2653f6d70e2cd1036
fedemotta <fedemotta@gmail.com>
2015-10-04 01:57:51 -0300

    Major refactor to setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,79 @@
-from distutils.core import setup
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
-  name = 'subseek',
-  packages = ['subseek'],
-  version = '2.0.7',
-  description = 'Search subtitles for all your movies and series with a single command line script',
-  author = 'Federico Nicolas Motta',
-  author_email = 'fedemotta@gmail.com',
-  url = 'https://github.com/fedemotta/subseek',
-  download_url = 'https://github.com/fedemotta/subseek/tarball/v2.0.7',
-  keywords = ['subtitles', 'movies', 'series','batch','script'],
-  classifiers = [],
+    name = 'subseek',
+
+    # Versions should comply with PEP440.  For a discussion on single-sourcing
+    # the version across setup.py and the project code, see
+    # https://packaging.python.org/en/latest/single_source_version.html
+    version='2.1',
+
+    # Download Url
+    download_url = 'https://github.com/fedemotta/subseek/tarball/v2.1',
+
+    description = 'Search subtitles for all your movies and series with a single command line script',
+    long_description=long_description,
+
+    # The project's main homepage.
+    url = 'https://github.com/fedemotta/subseek',
+  
+    # Author details
+    author = 'Federico Nicolas Motta',
+    author_email = 'fedemotta@gmail.com',
+  
+    # Choose your license
+    license='MIT',
+    
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 5 - Production/Stable',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: Utilities',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+    ],
+
+    # What does your project relate to?
+    keywords= 'subtitles movies series batch script',
+
+    # You can just specify the packages manually here if your project is
+    # simple. Or you can use find_packages().
+    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
+
+    # List run-time dependencies here.  These will be installed by pip when
+    # your project is installed. For an analysis of "install_requires" vs pip's
+    # requirements files see:
+    # https://packaging.python.org/en/latest/requirements.html
+    install_requires=['rarfile'],
+
+    # List additional groups of dependencies here (e.g. development
+    # dependencies). You can install these using the following syntax,
+    # for example:
+    # $ pip install -e .[dev,test]
+    extras_require={
+        'dev': ['check-manifest'],
+    },
 )
